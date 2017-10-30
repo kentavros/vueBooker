020d9962ffa6859bec1c37c01d0fcf64b8042b90
student <student@GFL-1465.ad.geeksforless.net>
2017-10-30 20:54:29 +0200

    1

---
+++ src/components/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main">
+  
+    Hello!
+    <button v-on:click="getMonth()">test</button>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Main',
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+      getMonth: function(){
+        var self = this
+        var date = new Date()
+        console.log(date.getMonth())
+        // console.log(date.getDay())
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Main from '@/components/Main'
+// import Registration from '@/components/Registration'
+// import Cart from '@/components/Cart'
+// import Admin from '@/components/Admin'
+// import AdminRegist from '@/components/AdminRegist'
+// import NewBook from '@/components/NewBook'
+// import AdminOrders from '@/components/AdminOrders'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Main',
+      component: Main
+      
+    }
+    // {
+    //   path: '/registration',
+    //   name: 'RegistrForm',
+    //   component: Registration
+    // },
+    // {
+    //   path: '/cart',
+    //   name: 'cartForm',
+    //   component: Cart
+    // },
+    // {
+    //   path: '/admin',
+    //   name: 'adminForm',
+    //   component: Admin,
+    //   children: [
+    //     {
+    //       path: 'regist',
+    //       component: AdminRegist
+    //     },
+    //     {
+    //       path: 'newbook',
+    //       component: NewBook
+    //     },
+    //     {
+    //       path: 'orders',
+    //       component: AdminOrders
+    //     }
+    //   ]
+    // }
+  ]
+})
